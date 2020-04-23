bdf9100beb481b8d4398ed96ac440ccf7e39c9e9
Mehboob750 <ms568203@gmail.com>
2020-04-23 15:59:23 +0530

    Added  Assign the letter 'O' to player and 'X' for the computer

--- TicTacToeSimulation.sh
+++ TicTacToeSimulation.sh
@@ -3,6 +3,8 @@
 BOARD_SIZE=3;
 BOARD_POSITION=10;
 INITIAL_SYMBOL="-";
+PLAYER_SYMBOL="O";
+COMPUTER_SYMBOL="X";
 echo "Welcome to Tic-Tac-Toe"
 for (( position=0; position<=$BOARD_POSITION; position++ ))
 do
