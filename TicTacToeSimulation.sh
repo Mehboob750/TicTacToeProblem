de8def7ccf9ff78418fb1cdb00fcc7c1fd27de39
Mehboob750 <ms568203@gmail.com>
2020-04-23 16:05:27 +0530

    Added Toss to check who plays first

--- TicTacToeSimulation.sh
+++ TicTacToeSimulation.sh
@@ -5,8 +5,19 @@ BOARD_POSITION=10;
 INITIAL_SYMBOL="-";
 PLAYER_SYMBOL="O";
 COMPUTER_SYMBOL="X";
+
+function resetBoard(){
 echo "Welcome to Tic-Tac-Toe"
 for (( position=0; position<=$BOARD_POSITION; position++ ))
 do
         BOARD[$position]=$INITIAL_SYMBOL;
 done
+}
+
+toss=$((RANDOM%2))
+if [ $toss -eq 1 ]
+then
+	echo "First turn is Player"
+else
+	echo "First Turn is Computer Turn"
+fi
